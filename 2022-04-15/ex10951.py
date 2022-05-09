537ce8cad702f37fa58ed982dcd41f3b29bf4594
MinSung Jung <minpro22@naver.com>
2022-05-09 18:53:08 +0900

    first commit

--- 2022-04-15/ex10951.py
+++ 2022-04-15/ex10951.py
@@ -2,8 +2,15 @@
 # A+B - 4
 
 import sys
-a = int(sys.stdin.readline())
 
-for i in range(a):
+"""for i in range(a):
     A, B = map(int, sys.stdin.readline().split())
+    print(A+B)
+"""
+
+while True:
+    try:
+        A, B = map(int, sys.stdin.readline().split())
+    except:
+        break
     print(A+B)
